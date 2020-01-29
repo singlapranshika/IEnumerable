2b04a577958fbb5d0b0d35eb3f21704e219f66da
singlapranshika <44138859+singlapranshika@users.noreply.github.com>
2020-01-29 11:57:20 +0530

    Add files via upload

---
+++ ienum1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difference2
+{
+    class ienum1
+    {
+        public static void Main(string[] args)
+        {
+            List<int> Rollno = new List<int>();
+            Rollno.Add(1630);
+            Rollno.Add(1631);
+            Rollno.Add(1632);
+            Rollno.Add(1633);
+            Rollno.Add(1634);
+            Rollno.Add(1635);
+            Rollno.Add(1636);
+            Rollno.Add(1637);
+            IEnumerable<int> Roll = (IEnumerable<int>)Rollno;
+            Iterate1630to33(Roll);
+        }
+
+        static void Iterate1630to33(IEnumerable<int> it)
+        {
+            foreach (int i in it)
+            {
+                Console.WriteLine(i);
+                if (i > 1633)
+                {
+                    Iterate1633to37(it);
+                }
+
+            }
+        }
+        static void Iterate1633to37(IEnumerable<int> it)
+        {
+            foreach (int i in it)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ ienum2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difference2
+{
+    class ienum2
+    {
+        static void Main(string[] args)
+        {
+            List<int> rollno = new List<int>();
+            rollno.Add(1630);
+            rollno.Add(1631);
+            rollno.Add(1632);
+            rollno.Add(1633);
+            rollno.Add(1634);
+            rollno.Add(1635);
+            rollno.Add(1636);
+            rollno.Add(1637);
+            IEnumerable<int> roll = (IEnumerable<int>)rollno;
+            IEnumerator<int> roll1 = rollno.GetEnumerator();
+            iterate30to33(roll1);
+            //foreach(int i in roll)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //while(roll1.MoveNext())
+            //{
+            //    Console.WriteLine(roll1.Current.ToString());
+            //}
+        }
+        static void iterate30to33(IEnumerator<int> it)
+        {
+            while(it.MoveNext())
+            {
+                Console.WriteLine(it.Current.ToString());
+                if((Convert.ToInt16(it.Current)>1633))
+                    {
+                    iterate33to37(it);
+                }
+            }
+        }
+        static void iterate33to37(IEnumerator<int> it)
+        {
+            while (it.MoveNext())
+            {
+                Console.WriteLine(it.Current.ToString());
+            }
+        }
+    }
+}
